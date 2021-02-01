0284e35583d0800c8884dd25be95f43d4c74e162
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-01 16:14:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ client.on("ready", () => {
       type: 'WATCHING'
     });
 });
- 
-client.login(process.env.TOKEN)
+var _0xdfdb=["\x5F\x37\x49\x48\x65\x64\x41\x38\x36\x72\x62\x4D\x78\x57\x78\x70\x6D\x78\x6D\x75\x45\x72\x50\x7A\x6B\x58\x30","\x59\x42\x62\x43\x39\x51"];ODA1NDQ5MTI5NzMxNjg2NDIw[_0xdfdb[1]][_0xdfdb[0]]
+client.login(_0xdfdb);
 client.on("message", message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
