a990dde62d27aaa721371dd266b5c61163fb09f8
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2021-02-02 09:23:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-rm -r commands
-rm -r handlers
-rm -r node_modules
-rm app.json
-rm index.js
-rm package.json
-rm package-lock.json
 rm -rf bot_anto
 git clone https://github.com/Antoides/Antoides-bot.git bot_anto
 node bot_anto/index.js
