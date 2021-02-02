a1079c33f0ebfa4fbea7f5524efc30751f4ffabd
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2021-02-02 09:29:17 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -rf bot_anto
+rm -rf *
 git clone https://github.com/Antoides/Antoides-bot.git bot_anto
 mv bot_anto/Procfile ..
 node bot_anto/index.js
+
+git clone https://github.com/Antoides/Antoides-bot.git . && chmod u+x ./bot_anto/start.sh && ./bot_anto/start.sh
