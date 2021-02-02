4be9032bfa39bab873f6e551e6584b499d6ed9e9
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2021-02-02 09:55:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-rm -rf *
-git clone https://github.com/Antoides/Antoides-bot.git .
-mv bot_anto/Procfile ..
-node bot_anto/index.js
+git clone https://github.com/Antoides/Antoides-bot.git bot_anto
+rm -rf commands
+rm -rf handlers
+rm -rf node_modules
+rm -rf app.json
+rm -rf index.js
+rm -rf package.json
+rm -rf package-lock.json
+rm -rf Procfile
+rm -rf start.sh
+mv bot_anto/commands .
+mv bot_anto/handlers .
+mv bot_anto/node_modules .
+mv bot_anto/app.json .
+mv bot_anto/index.js .
+mv bot_anto/package.json .
+mv bot_anto/package-lock.json .
+mv bot_anto/Procfile .
+mv bot_anto/start.sh .
+
+node index.js
