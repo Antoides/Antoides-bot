b00369276911d485ad1613627483fa6401620375
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-03 15:32:23 +0100

    Create lista.js

---
+++ commands/varie/lista.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "lista",
+    aliases: ["list"],
+    category: "messaggi",
+    description: "Mostra la lista dei comandi",
+    run: async (client, message, args) => {
+     
+      if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        return message.reply("**Lista comandi: %meme(manda un meme), %clear(elimina i messaggi), %list(lista dei comandi)**")
+      }
+    }
+};
