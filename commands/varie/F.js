5de0ecda567d6ee3c45b030dd4c2dd089c6a577a
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-04 22:06:46 +0100

    Create F.js

---
+++ commands/varie/F.js
@@ -0,0 +1,19 @@
+const Embed = require("embedcord");
+const Discord = require("discord.js");
+const { promptMessage } = require("../../functions.js");
+ 
+const chooseArr = ["[F]"];
+ 
+module.exports = {
+    name: "F",
+    description: "Press F to pay respect",
+    usage: "F",
+    run: async (client, message, args) => {
+      const embed = new Embed.DiscordEmbed()
+          .setColor('#666666')
+          .setFooter(message.guild.me.displayName, client.user.displayAvatarURL)
+          .setDescription("**PRESS [F] TO PAY RESPECT**")
+          .setTimestamp();
+      }
+ 
+    }
