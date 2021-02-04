48a57e7cd34f5dcf10d383ba82b0ffa89661c61c
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2021-02-04 22:04:42 +0100

    Update list.js

--- commands/varie/list.js
+++ commands/varie/list.js
@@ -10,8 +10,8 @@ module.exports = {
     run: async (client, message, args) => {
 	
 	  var msgs = "I comandi del bot sono:\n";
-      readdirSync("../../commands/").forEach(dir => {
-        const commands = readdirSync(`../../commands/varie/${dir}/`).filter(file => file.endsWith(".js"));
+      readdirSync("../commands/").forEach(dir => {
+        const commands = readdirSync(`../commands/varie/${dir}/`).filter(file => file.endsWith(".js"));
 		for (let file of commands) {
 			let pull = require(`../commands/varie/${dir}/${file}`);
 			var cmd = pull.aliases[0];
