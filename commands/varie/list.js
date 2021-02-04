c68fa659e651fc32dd2b01dea31f5946b06b0b89
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-04 21:55:02 +0100

    Update list.js

--- commands/varie/list.js
+++ commands/varie/list.js
@@ -11,9 +11,9 @@ module.exports = {
 	
 	  var msgs = "I comandi del bot sono:\n";
       readdirSync("../../commands/").forEach(dir => {
-        const commands = readdirSync(`../../commands/${dir}/`).filter(file => file.endsWith(".js"));
+        const commands = readdirSync(`../../commands/varie/${dir}/`).filter(file => file.endsWith(".js"));
 		for (let file of commands) {
-			let pull = require(`../../commands/${dir}/${file}`);
+			let pull = require(`../../commands/varie/${dir}/${file}`);
 			var cmd = pull.aliases[0];
 			var desc = pull.description;
 			var msgs = cmd + ": " + desc + "\n";
