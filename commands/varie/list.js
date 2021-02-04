d31332f7e841c30e202536ad03884c5f11e83031
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-04 21:33:26 +0100

    Create list.js

---
+++ commands/varie/list.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "lista",
+    aliases: ["list"],
+    category: "messaggi",
+    description: "Mostra la lista dei comandi",
+    run: async (client, message, args) => {
+	
+	  var msgs = "I comandi del bot sono:\n";
+      readdirSync("../../commands/").forEach(dir => {
+        const commands = readdirSync(`../../commands/${dir}/`).filter(file => file.endsWith(".js"));
+		for (let file of commands) {
+			let pull = require(`../../commands/${dir}/${file}`);
+			var cmd = pull.aliases[0];
+			var desc = pull.description;
+			var msgs = cmd + ": " + desc + "\n";
+		}
+      });
+      return message.reply(msgs)
+    }
+};
