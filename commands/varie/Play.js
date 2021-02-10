6dad5d1f97c5d13030c9363cbf15cfeb5a6ef628
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-10 10:14:53 +0100

    Update Play.js

--- commands/varie/Play.js
+++ commands/varie/Play.js
@@ -5,14 +5,12 @@ module.exports = {
     description: "Lancia una nuke indiana",
 	run: async (client, message, args) => {
 	
-	if (message.content == "%play") {
     const voiceChannel = message.member.voice.channel;
     if (voiceChannel) {
         voiceChannel.join()
             .then(connection => {
-                connection.play('Antoides-bot/WhatsApp Audio 2021-02-10 at 08.45.11.mpeg'); //Scrivere il nome del file audio nella cartella o il path
+                connection.play('Antoides-bot\WhatsApp Audio 2021-02-10 at 08.45.11.mpeg'); //Scrivere il nome del file audio nella cartella o il path
             });
-    }
     else {
         message.channel.send("Entra in una chat vocale, coglione"); //Messaggio se l'utente non è in nessun canale vocale
     }
