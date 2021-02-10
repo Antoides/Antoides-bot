56a9368c2c69107a57ad8c9d7587a5070238b80e
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-10 10:00:24 +0100

    play

--- commands/varie/Play.js
+++ commands/varie/Play.js
@@ -2,21 +2,5 @@ module.exports = {
     name: "play",
     aliases: ["p"],
     category: "voice",
-    description: "Lancia una nuke indiana",
-	run: async (client, message, args) => {
-	
-	if (message.content == "%play") {
-    const voiceChannel = message.member.voice.channel;
-    if (voiceChannel) {
-        voiceChannel.join()
-            .then(connection => {
-                connection.play('WhatsApp Audio 2021-02-10 at 08.45.11.mpeg'); //Scrivere il nome del file audio nella cartella o il path
-            });
-    }
-    else {
-        message.channel.send("Entra in una chat vocale, coglione"); //Messaggio se l'utente non è in nessun canale vocale
-    }
-}
-	}
-	
+    description: "Lancia una nuke indiana"
 }
