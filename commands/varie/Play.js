43c2386ce4b0d876ac7f2a26215a3b676a10ed15
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-10 10:29:38 +0100

    Update Play.js

--- commands/varie/Play.js
+++ commands/varie/Play.js
@@ -12,6 +12,7 @@ module.exports = {
             .then(connection => {
                 connection.play('loud indian music (lol mega earrape)(MP3_160K).mp3'); //Scrivere il nome del file audio nella cartella o il path
             });
+	}
     else {
         message.channel.send("Entra in una chat vocale, coglione"); //Messaggio se l'utente non è in nessun canale vocale
     }
