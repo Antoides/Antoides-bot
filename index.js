e59f61d499eb7630936db75cfa696db096d5fe4d
Antoides <78360130+Antoides@users.noreply.github.com>
2021-02-10 10:04:28 +0100

    play

--- index.js
+++ index.js
@@ -64,16 +64,3 @@ client.on('guildMemberAdd', member => {
  if (!channel) return;
  channel.send(`$(member), **Benvenuto nel server di Antoides!**`);
 });
-
-if (message.content == "%play") {
-    const voiceChannel = message.member.voice.channel;
-    if (voiceChannel) {
-        voiceChannel.join()
-            .then(connection => {
-                connection.play('WhatsApp Audio 2021-02-10 at 08.45.11.mpeg'); //Scrivere il nome del file audio nella cartella o il path
-            });
-    }
-    else {
-        message.channel.send("Entra in una chat vocale, coglione"); //Messaggio se l'utente non è in nessun canale vocale
-    }
-}
